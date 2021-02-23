8b68d9677cf5064068d1ed91f55fc2f703de4abd
jojiapp <jojiapp@gmail.com>
2021-02-23 12:46:01 +0900

    join

--- src/main/java/com/yts8/sixuniverse/home/HomeController.java
+++ src/main/java/com/yts8/sixuniverse/home/HomeController.java
@@ -1,13 +1,13 @@
-package com.yts8.sixuniverse.security;
+package com.yts8.sixuniverse.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SecurityController {
+public class HomeController {
 
   @GetMapping("/")
-  public String index() {
+  public String home() {
     return "index";
   }
 }
