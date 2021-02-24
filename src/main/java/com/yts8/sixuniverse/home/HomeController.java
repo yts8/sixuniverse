9a2ff86a560b9cb5b85867bd0b9ea1964fbedece
jojiapp <72853422+jojiapp@users.noreply.github.com>
2021-02-24 10:58:28 +0900

    default layout (#5)
    기본 레이아웃 틀

--- src/main/java/com/yts8/sixuniverse/home/HomeController.java
+++ src/main/java/com/yts8/sixuniverse/home/HomeController.java
@@ -1,13 +1,15 @@
 package com.yts8.sixuniverse.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
   @GetMapping("/")
-  public String home() {
-    return "index";
+  public String home(Model model) {
+    model.addAttribute("title", "Home");
+    return "home/index";
   }
 }
