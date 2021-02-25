53ceae48f69cd7910be78125e0dd8c82182e90f2
somsomy <78060557+somsomy@users.noreply.github.com>
2021-02-25 21:23:18 +0900

    reservtion view (#15)

---
+++ src/main/java/com/yts8/sixuniverse/reservation/controller/ReservationController.java
@@ -0,0 +1,16 @@
+package com.yts8.sixuniverse.reservation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reservation")
+public class ReservationController {
+
+  @GetMapping("")
+  public String reservation() {
+
+    return "reservation/index";
+  }
+}
