64d95e8e3428344e6eba68cb24377a01222add53
som <yun07003@naver.com>
2021-02-25 21:18:33 +0900

    reservtion view

---
+++ src/main/resources/static/js/reservation/reservation-modal.js
@@ -0,0 +1,13 @@
+(() => {
+  $(document).ready(function () {
+    $('#update-guest').bind('click', function () {
+      $('#my-modal').show();
+    });
+
+    $('.reservation__modal-cancel').bind('click', function () {
+      $('#my-modal').hide();
+    });
+
+  });
+
+})()
