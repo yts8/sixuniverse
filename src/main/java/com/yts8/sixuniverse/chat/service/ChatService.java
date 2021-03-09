e1c7e167b553931cb108fdc2f6d24e48b5dab31d
seorin2 <rtf1030@naver.com>
2021-03-10 01:27:41 +0900

    chat test, css update

--- src/main/java/com/yts8/sixuniverse/chat/service/ChatService.java
+++ src/main/java/com/yts8/sixuniverse/chat/service/ChatService.java
@@ -7,13 +7,10 @@ import com.yts8.sixuniverse.chat.dto.ChatroomJoinDto;
 public interface ChatService {
 
   /* 채팅메세지 보내기*/
-  void addMessage(ChatDto chatDto);
+   void addMessage(ChatDto chatDto);
 
   /* 채팅방 생성용 메서드*/
   void createChatroomJoin(ChatroomJoinDto chatroomJoinDto);
 
-  /* 채팅방 존재 여부*/
-  // 내 memberId로 chat테이블의 content가 있는지 확인
-  // 같은 chat_ref를 참조하는
-  ChatDto findMessage(Long memberId);
+
 }
