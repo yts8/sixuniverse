53f1905984058fa6fa243e20a1ea7f953397c083
star <kingbar222@naver.com>
2021-03-19 19:17:57 +0900

    room detail update

--- src/main/java/com/yts8/sixuniverse/room/controller/RoomController.java
+++ src/main/java/com/yts8/sixuniverse/room/controller/RoomController.java
@@ -1,9 +1,18 @@
 package com.yts8.sixuniverse.room.controller;
 
+import com.yts8.sixuniverse.member.dto.MemberDto;
+import com.yts8.sixuniverse.room.dto.RoomDto;
 import com.yts8.sixuniverse.room.service.RoomService;
+import com.yts8.sixuniverse.roomImage.service.RoomImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,4 +20,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RoomController {
 
   private final RoomService roomService;
+  private final RoomImageService roomImageService;
+
+  @GetMapping("/detail/{roomId}")
+  public String detail(HttpSession session, HttpServletRequest request, Model model, @PathVariable Long roomId) {
+    model.addAttribute("title", "숙소 상세 정보");
+
+    MemberDto member = (MemberDto) session.getAttribute("member");
+    RoomDto roomDto = roomService.findById(roomId);
+    model.addAttribute("room", roomDto);
+
+//    model.addAttribute("hostId", getHostId());
+
+    model.addAttribute("roomImages", roomImageService.findByRoomId(roomDto.getRoomId()));
+    return "room/detail";
+  }
+
+
 }
