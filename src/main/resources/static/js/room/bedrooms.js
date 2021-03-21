0c96c3eb28100b83ecf908e389d3601eced7c975
star <kingbar222@naver.com>
2021-03-21 23:37:47 +0900

    room UI update

--- src/main/resources/static/js/room/bedrooms.js
+++ src/main/resources/static/js/room/bedrooms.js
@@ -8,7 +8,7 @@
   const handleSubmitClick = (e) => {
     if (!(maxPeopleEl.value > 0)) {
       e.preventDefault();
-      alert("최대 숙박 인원은 1 이상이여야 합니다.");
+      alert("최대 숙박 인원은 1 이상이어야 합니다.");
     }
   }
 
