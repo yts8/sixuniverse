a85b922c0a6e6eefe88e55388f258b0170d92311
jojiapp <72853422+jojiapp@users.noreply.github.com>
2021-03-25 06:54:42 +0900

    room readcount (#104)

---
+++ src/main/java/com/yts8/sixuniverse/roomHits/repository/RoomHitsMapper.java
@@ -0,0 +1,15 @@
+package com.yts8.sixuniverse.roomHits.repository;
+
+import com.yts8.sixuniverse.roomHits.dto.RoomHitsDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RoomHitsMapper {
+
+  void save(RoomHitsDto roomHitsDto);
+
+  RoomHitsDto findByRoomIdAndReadDate(RoomHitsDto roomHitsDto);
+
+  void updateHits(Long roomHitsId);
+
+}
