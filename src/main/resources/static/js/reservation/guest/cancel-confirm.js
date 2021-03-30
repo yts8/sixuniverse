3e5781f20a6e5e10549db32cb613ee8cef784682
som <yun07003@naver.com>
2021-03-30 13:41:59 +0900

    payment cancel

--- src/main/resources/static/js/reservation/guest/cancel-confirm.js
+++ src/main/resources/static/js/reservation/guest/cancel-confirm.js
@@ -9,29 +9,29 @@
 
       const paymentId = $('#payment-id').val();
 
-      // cancelPay();
-      //
-      // function cancelPay() {
-      //   $.ajax({
-      //     url: `//${location.host}/api/reservation/guest/pay/cancel`,
-      //     type: 'post',
-      //     data: JSON.stringify({
-      //       imp_uid: paymentId
-      //     }),
-      //     contentType: 'application/json; charset=utf-8',
-      //     beforeSend: function (xhr) {
-      //       xhr.setRequestHeader(header, csrf);
-      //     }
-      //   }).done(function (result) { // 환불 성공시 로직
-      //     alert("환불 성공");
-      //     cancelDelete();
-      //   }).fail(function (error) { // 환불 실패시 로직
-      //     alert("환불 실패");
-      //   });
-      // }
+      cancelPay();
+
+      function cancelPay() {
+        $.ajax({
+          url: `//${location.host}/api/reservation/guest/pay/cancel`,
+          type: 'post',
+          data: JSON.stringify({
+            imp_uid: paymentId
+          }),
+          contentType: 'application/json; charset=utf-8',
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader(header, csrf);
+          }
+        }).done(function (result) { // 환불 성공시 로직
+          alert("환불 성공");
+          cancelDelete();
+        }).fail(function (error) { // 환불 실패시 로직
+          alert("환불 실패");
+        });
+      }
 
       // -------------------
-      cancelDelete()     // 주석 처리 후 다른 주석 해제하면 결제 취소 가능
+      // cancelDelete()     // 주석 처리 후 다른 주석 해제하면 결제 취소 가능
       // -------------------
 
       function cancelDelete() {
@@ -47,6 +47,7 @@
             xhr.setRequestHeader(header, csrf);
           },
           success: function () {
+            alert('취소되었습니다.');
             $('.guest-reservation-cancel__title').html('예약이 취소되었습니다.');
             $('.guest-reservation-cancel__back-btn').css('display', 'none');
             $('.guest-reservation-cancel__next-btn').css('display', 'none');
